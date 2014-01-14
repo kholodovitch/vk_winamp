85c50691a2dacdc03c6fa6beac053bf2f043d590
Alexander Kholodovitch <kholodovitch@gmail.com>
2014-01-15 02:02:26 +0400

    Parse and send information of authorization form.

--- src/api/ApiCore/Utils/Authorization/OAuth/OAuthHidden.cs
+++ src/api/ApiCore/Utils/Authorization/OAuth/OAuthHidden.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ApiCore.Utils.Authorization
 {
@@ -25,32 +28,63 @@ namespace ApiCore.Utils.Authorization
 			string startUrl = string.Format("http://oauth.vk.com/oauth/authorize?redirect_uri={0}&response_type=token&client_id={1}&scope={2}&display=wap&no_session=1", blankHtml, appId, scope);
 
 			byte[] content = GetResponse(startUrl);
-			var authorizeHtml = Encoding.UTF8.GetString(content);
+			string authorizeHtml = Encoding.UTF8.GetString(content);
 
 			// remove script element for prevent redirecting to remote page
-			var authorizeHtmlLocal = authorizeHtml.Substring(0, authorizeHtml.IndexOf("<script")) + authorizeHtml.Substring(authorizeHtml.IndexOf("</script>") + "</script>".Length);
+			string authorizeHtmlLocal = authorizeHtml.Substring(0, authorizeHtml.IndexOf("<script")) + authorizeHtml.Substring(authorizeHtml.IndexOf("</script>") + "</script>".Length);
 			authorizeHtmlLocal = authorizeHtmlLocal.Replace("href=\"/", "href=\"http://oauth.vk.com/");
 			File.WriteAllText("index.html", authorizeHtmlLocal);
 
 			string formStart = "<form";
-			int startFormTag = authorizeHtml.IndexOf(formStart) - formStart.Length;
+			string formEnd = "</form>";
+			int startFormTag = authorizeHtml.IndexOf(formStart);
+			int endFormTag = authorizeHtml.IndexOf(formEnd);
+			string formHtml = authorizeHtml.Substring(startFormTag, endFormTag + formEnd.Length - startFormTag);
+			string formAction = Regex.Match(formHtml, @"action\=\""(.*)\""").Groups[1].Value;
+
+			const string formParamsPattern = @"\<input .+ name=\""(.*)\"" value=\""(.*)\"".*>";
+			var foundFormParams = Regex.Matches(formHtml, formParamsPattern);
+			var formParams = new Dictionary<string, string>();
+			foreach (Match match in foundFormParams)
+				formParams[match.Groups[1].Value] = match.Groups[2].Value;
+			formParams["email"] = _email;
+			formParams["pass"] = _pass;
+
+			byte[] postContent = GetResponse(formAction, x => WritePostData(x, formParams));
+			string postHtml = Encoding.UTF8.GetString(postContent);
+			File.WriteAllText("post.html", postHtml);
 
 			return SessionData;
 		}
 
-		private static byte[] GetResponse(string startUrl)
+		private static void WritePostData(HttpWebRequest request, Dictionary<string, string> formParams)
+		{
+			request.Method = "POST";
+			request.ContentType = "application/x-www-form-urlencoded";
+
+			byte[] byteArray = Encoding.UTF8.GetBytes(DictToString(formParams));
+			request.ContentLength = byteArray.Length;
+			var datastream = request.GetRequestStream();
+			datastream.Write(byteArray, 0, byteArray.Length);
+			datastream.Close();
+		}
+
+		private static byte[] GetResponse(string startUrl, Action<HttpWebRequest> requestAction = null)
 		{
 			var request = (HttpWebRequest) WebRequest.Create(startUrl);
 
 			request.UserAgent = @"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)";
 			request.CookieContainer = Container;
 
+			if (requestAction != null)
+				requestAction(request);
+
 			return GetRetArray(request);
 		}
 
 		private static byte[] GetRetArray(WebRequest request)
 		{
-			const int maxPacketByteSize = 64 * 1024;
+			const int maxPacketByteSize = 64*1024;
 			byte[] retArray = null;
 
 			using (WebResponse response = request.GetResponse())
@@ -74,6 +108,16 @@ namespace ApiCore.Utils.Authorization
 				}
 			}
 			return retArray;
+		}
+
+		private static string DictToString(Dictionary<string, string> dict)
+		{
+			var builder = new StringBuilder();
+
+			foreach (KeyValuePair<string, string> kvp in dict)
+				builder.Append(kvp.Key + "=" + kvp.Value + "&");
+
+			return builder.ToString();
 		}
 	}
 }
