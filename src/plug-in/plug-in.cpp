c25d3c5beb3f77dde4cfc5890d05ba9ecf2302f3
Alexander Kholodovitch <kholodovitch@gmail.com>
2014-05-22 14:27:01 +0400

    Add sample methods for loading and configuring plug-in.

--- src/plug-in/plug-in.cpp
+++ src/plug-in/plug-in.cpp
@@ -4,3 +4,44 @@
 
 #include "plug-in.h"
 
+
+// these are callback functions/events which will be called by Winamp
+int  init(void);
+void config(void);
+void quit(void);
+
+
+// this structure contains plugin information, version, name...
+// GPPHDR_VER is the version of the winampGeneralPurposePlugin (GPP) structure
+winampGeneralPurposePlugin plugin = {
+	GPPHDR_VER,  // version of the plugin, defined in "gen_myplugin.h"
+	PLUGIN_NAME, // name/title of the plugin, defined in "gen_myplugin.h"
+	init,        // function name which will be executed on init event
+	config,      // function name which will be executed on config event
+	quit,        // function name which will be executed on quit event
+	0,           // handle to Winamp main window, loaded by winamp when this dll is loaded
+	0            // hinstance to this dll, loaded by winamp when this dll is loaded
+};
+
+
+// event functions follow
+
+int init() {
+	System::Windows::Forms::MessageBox::Show("Init event triggered for gen_myplugin. Plugin installed successfully!");
+	return 0;
+}
+
+void config() {
+	System::Windows::Forms::MessageBox::Show("Config event triggered for gen_myplugin.");
+}
+
+void quit() {
+	System::Windows::Forms::MessageBox::Show("Quit event triggered for gen_myplugin.");
+}
+
+
+// This is an export function called by winamp which returns this plugin info.
+// We wrap the code in 'extern "C"' to ensure the export isn't mangled if used in a CPP file.
+extern "C" __declspec(dllexport) winampGeneralPurposePlugin * winampGetGeneralPurposePlugin() {
+	return &plugin;
+}
--- src/plug-in/plug-in.h
+++ src/plug-in/plug-in.h
@@ -1,13 +1,21 @@
-// plug-in.h
-
 #pragma once
 
-using namespace System;
+#include <windows.h>
 
-namespace plugin {
+// plugin version (don't touch this)
+#define GPPHDR_VER 0x10
 
-	public ref class Class1
-	{
-		// TODO: Add your methods for this class here.
-	};
-}
+// plugin name/title (change this to something you like)
+#define PLUGIN_NAME "My first generic Winamp plugin!"
+
+
+// main structure with plugin information, version, name...
+typedef struct {
+	int version;                   // version of the plugin structure
+	char *description;             // name/title of the plugin 
+	int (*init)();                 // function which will be executed on init event
+	void (*config)();              // function which will be executed on config event
+	void (*quit)();                // function which will be executed on quit event
+	HWND hwndParent;               // hwnd of the Winamp client main window (stored by Winamp when dll is loaded)
+	HINSTANCE hDllInstance;        // hinstance of this plugin DLL. (stored by Winamp when dll is loaded) 
+} winampGeneralPurposePlugin;
